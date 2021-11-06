33a129ffdeaff0198f766cd2fc9ff66552bab98c
Allan F Ribeiro <allanfribeiro@gmail.com>
2021-11-06 15:55:21 -0300

    Início do projeto

---
+++ comprasCupom/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace comprasCupom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vCompra, tCompra;
+            string cupom;
+
+            Console.WriteLine("Digite o valor da compra");
+            vCompra = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o cupom");
+            cupom = Console.ReadLine().ToUpper();
+
+            if(vCompra > 1000 || cupom == "DESCONTO20")
+            {
+                tCompra = vCompra - (vCompra * 0.20);
+            }
+            else
+            {
+                tCompra = vCompra;
+            }
+            Console.WriteLine("O Total da compra é de R$ {0}", tCompra.ToString("0.00"));
+        }
+    }
+}
